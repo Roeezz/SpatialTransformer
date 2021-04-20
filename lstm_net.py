bea4a9aeac7cd34ffb081ce9ca3733ec36eb4bb8
Roee Weiss Lipshitz <39015778+Roeezz@users.noreply.github.com>
2021-04-20 11:29:49 +0300

    Add lstm, lables and SSIM for loss of the lstm.

---
+++ lstm_net.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+        self.lstm = nn.LSTM(500, 32, num_layers=1, batch_first=True)
+
+        # Spatial transformer localization-network
+        ngf = 128
+        self.localization = nn.Sequential(
+            nn.Conv2d(57, ngf, kernel_size=(3, 3), stride=1),
+            nn.BatchNorm2d(ngf),
+            nn.LeakyReLU(True),
+            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
+
+            nn.Conv2d(ngf, ngf * 2, kernel_size=(3, 3), stride=1),
+            nn.BatchNorm2d(ngf * 2),
+            nn.LeakyReLU(True),
+            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
+
+            nn.Conv2d(ngf * 2, ngf * 4, kernel_size=(3, 3), stride=1),
+            nn.BatchNorm2d(ngf * 4),
+            nn.LeakyReLU(True),
+            nn.Conv2d(ngf * 4, ngf * 4, kernel_size=(3, 3), stride=1),
+            nn.BatchNorm2d(ngf * 4),
+            nn.LeakyReLU(True),
+            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
+
+            nn.Conv2d(ngf * 4, ngf * 8, kernel_size=(3, 3), stride=1),
+            nn.BatchNorm2d(ngf * 8),
+            nn.LeakyReLU(True),
+            nn.Conv2d(ngf * 8, ngf * 8, kernel_size=(3, 3), stride=1),
+            nn.BatchNorm2d(ngf * 8),
+            nn.LeakyReLU(True),
+            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
+
+            nn.Conv2d(ngf * 8, 500, kernel_size=(3, 3), stride=1),
+            nn.BatchNorm2d(500),
+            nn.LeakyReLU(True),
+            nn.Conv2d(500, 500, kernel_size=(3, 3), stride=1),
+            nn.BatchNorm2d(500),
+            nn.LeakyReLU(True),
+
+            nn.AvgPool2d(kernel_size=(7, 23), stride=(7, 23))
+        )
+
+        # Regressor for the 3 * 2 affine matrix
+        self.fc_loc = nn.Sequential(
+            nn.Linear(500, 32),
+            nn.LeakyReLU(0.2, True),
+            nn.Linear(32, 3 * 2)
+        )
+
+        # Initialize the weights/bias with identity transformation
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
+    # Spatial transformer network forward function
+    def lstm(self, x, x_last):
+        xs = self.localization(x)
+        xs = xs.view(-1, 500)
+        xs = self.fc_loc(xs)
+
+        return x
+
+    def forward(self, x, x_flow):
+        # transform the input
+        x_last = x[:, :, -1, :, :]
+
+        x = x.view(x.shape[0], -1, 256, 512)
+        x_flow = x_flow.view(x_flow.shape[0], -1, 256, 512)
+        x = torch.cat((x, x_flow), dim=1)
+
+        x = self.lstm(x, x_last)
+
+        return x
