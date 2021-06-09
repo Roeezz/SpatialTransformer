c80ea6c2e820b3660dce5ac027492fea62a0ac8e
Roee Weiss Lipshitz <39015778+Roeezz@users.noreply.github.com>
2021-06-09 15:23:05 +0300

    change the nets to fit the smaller pictures

--- lstm_net.py
+++ lstm_net.py
@@ -8,9 +8,9 @@ class LSTM(nn.Module):
         super(LSTM, self).__init__()
         self.lstm = nn.LSTM(82, 82, num_layers=1)
 
-        # Spatial transformer localization-network
+        # convolution for the pictures
         ngf = 128
-        self.localization = nn.Sequential(
+        self.encoder = nn.Sequential(
             nn.Conv2d(51, ngf, kernel_size=(3, 3), stride=1),
             nn.BatchNorm2d(ngf),
             nn.LeakyReLU(True),
@@ -29,86 +29,41 @@ class LSTM(nn.Module):
             nn.LeakyReLU(True),
             nn.MaxPool2d(kernel_size=(3, 3), stride=2),
 
-            nn.Conv2d(ngf * 4, ngf * 8, kernel_size=(3, 3), stride=1),
-            nn.BatchNorm2d(ngf * 8),
-            nn.LeakyReLU(True),
-            nn.Conv2d(ngf * 8, ngf * 8, kernel_size=(3, 3), stride=1),
-            nn.BatchNorm2d(ngf * 8),
-            nn.LeakyReLU(True),
-            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
+            # nn.Conv2d(ngf * 4, ngf * 4, kernel_size=(3, 3), stride=1),
+            # nn.BatchNorm2d(ngf * 4),
+            # nn.LeakyReLU(True),
+            # nn.Conv2d(ngf * 8, ngf * 8, kernel_size=(3, 3), stride=1),
+            # nn.BatchNorm2d(ngf * 8),
+            # nn.LeakyReLU(True),
+            # nn.MaxPool2d(kernel_size=(3, 3), stride=2),
 
-            nn.Conv2d(ngf * 8, 500, kernel_size=(3, 3), stride=1),
+            nn.Conv2d(ngf * 4, 500, kernel_size=(3, 3), stride=1),
             nn.BatchNorm2d(500),
             nn.LeakyReLU(True),
             nn.Conv2d(500, 500, kernel_size=(3, 3), stride=1),
             nn.BatchNorm2d(500),
             nn.LeakyReLU(True),
 
-            nn.AvgPool2d(kernel_size=(7, 23), stride=(7, 23))
-        )
-
-        # convolution for the pictures
-        self.input_encoder = nn.Sequential(
-            nn.Conv2d(3, ngf, kernel_size=(3, 3), stride=1),
-            nn.BatchNorm2d(ngf),
-            nn.LeakyReLU(True),
-            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
-
-            nn.Conv2d(ngf, ngf * 2, kernel_size=(3, 3), stride=1),
-            nn.BatchNorm2d(ngf * 2),
-            nn.LeakyReLU(True),
-            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
-
-            nn.Conv2d(ngf * 2, ngf * 4, kernel_size=(3, 3), stride=1),
-            nn.BatchNorm2d(ngf * 4),
-            nn.LeakyReLU(True),
-            nn.Conv2d(ngf * 4, ngf * 4, kernel_size=(3, 3), stride=1),
-            nn.BatchNorm2d(ngf * 4),
-            nn.LeakyReLU(True),
-            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
-
-            nn.Conv2d(ngf * 4, ngf * 8, kernel_size=(3, 3), stride=1),
-            nn.BatchNorm2d(ngf * 8),
-            nn.LeakyReLU(True),
-            nn.Conv2d(ngf * 8, ngf * 8, kernel_size=(3, 3), stride=1),
-            nn.BatchNorm2d(ngf * 8),
-            nn.LeakyReLU(True),
-            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
-
-            nn.Conv2d(ngf * 8, 500, kernel_size=(3, 3), stride=1),
-            nn.BatchNorm2d(500),
-            nn.LeakyReLU(True),
-            nn.Conv2d(500, 500, kernel_size=(3, 3), stride=1),
-            nn.BatchNorm2d(500),
-            nn.LeakyReLU(True),
-
-            nn.AvgPool2d(kernel_size=(7, 23), stride=(7, 23))
-        )
-
-        self.fc_loc = nn.Sequential(
-            nn.Linear(500, 128),
-            nn.LeakyReLU(0.2, True),
-            nn.Linear(128, 87)
+            nn.AvgPool2d(kernel_size=(17, 32), stride=(17, 32))
         )
 
         self.fc_enc = nn.Sequential(
-            nn.Linear(500, 64),
+            nn.Linear(500, 128),
             nn.LeakyReLU(0.2, True),
-            nn.Linear(128, 87)
+            nn.Linear(128, 82)
         )
 
     def forward(self, xs, xs_flow, label, label_pred):
         # transform the input
         batch_sz = label_pred.shape[1]
         seq_sz = label_pred.shape[0]
-
-        xs = xs.reshape(batch_sz, -1, 256, 512)
-        xs_flow = xs_flow.reshape(batch_sz, -1, 256, 512)
+        xs = xs.reshape(batch_sz, -1, 200, 320)
+        xs_flow = xs_flow.reshape(batch_sz, -1, 200, 320)
         xs = torch.cat((xs, xs_flow), dim=1)
 
-        hidden = self.localization(xs)
+        hidden = self.encoder(xs)
         hidden = hidden.reshape(-1, 500)
-        hidden = self.fc_loc(hidden).view(1, batch_sz, 82)
+        hidden = self.fc_enc(hidden).view(1, batch_sz, 82)
         out = label
         hidden_in = (hidden, hidden)
         for i in range(seq_sz):
--- stn_net.py
+++ stn_net.py
@@ -31,22 +31,23 @@ class STN(nn.Module):
             nn.LeakyReLU(True),
             nn.MaxPool2d(kernel_size=(3, 3), stride=2),
 
-            nn.Conv2d(ngf * 4, ngf * 8, kernel_size=(3, 3), stride=1),
-            nn.BatchNorm2d(ngf * 8),
-            nn.LeakyReLU(True),
-            nn.Conv2d(ngf * 8, ngf * 8, kernel_size=(3, 3), stride=1),
-            nn.BatchNorm2d(ngf * 8),
-            nn.LeakyReLU(True),
-            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
+            # this was meant to be used for the scaled up pictures, now we use smaller pictures
+            # nn.Conv2d(ngf * 4, ngf * 8, kernel_size=(3, 3), stride=1),
+            # nn.BatchNorm2d(ngf * 8),
+            # nn.LeakyReLU(True),
+            # nn.Conv2d(ngf * 8, ngf * 8, kernel_size=(3, 3), stride=1),
+            # nn.BatchNorm2d(ngf * 8),
+            # nn.LeakyReLU(True),
+            # nn.MaxPool2d(kernel_size=(3, 3), stride=2),
 
-            nn.Conv2d(ngf * 8, 500, kernel_size=(3, 3), stride=1),
+            nn.Conv2d(ngf * 4, 500, kernel_size=(3, 3), stride=1),
             nn.BatchNorm2d(500),
             nn.LeakyReLU(True),
             nn.Conv2d(500, 500, kernel_size=(3, 3), stride=1),
             nn.BatchNorm2d(500),
             nn.LeakyReLU(True),
 
-            nn.AvgPool2d(kernel_size=(7, 23), stride=(7, 23))
+            nn.AvgPool2d(kernel_size=(17, 32), stride=(17, 32))
         )
 
         # Regressors for the 3 * 2 affine matrix
@@ -61,39 +62,12 @@ class STN(nn.Module):
             nn.Linear(32, 3 * 2)
         )
 
-        # self.fc_loc3 = nn.Sequential(
-        #     nn.Linear(500, 32),
-        #     nn.LeakyReLU(0.2, True),
-        #     nn.Linear(32, 3 * 2)
-        # )
-        #
-        # self.fc_loc4 = nn.Sequential(
-        #     nn.Linear(500, 32),
-        #     nn.LeakyReLU(0.2, True),
-        #     nn.Linear(32, 3 * 2)
-        # )
-        #
-        # self.fc_loc5 = nn.Sequential(
-        #     nn.Linear(500, 32),
-        #     nn.LeakyReLU(0.2, True),
-        #     nn.Linear(32, 3 * 2)
-        # )
-
         # Initialize the weights/bias with identity transformation
         self.fc_loc1[2].weight.data.zero_()
         self.fc_loc1[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
 
         self.fc_loc2[2].weight.data.zero_()
         self.fc_loc2[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
-
-        # self.fc_loc3[2].weight.data.zero_()
-        # self.fc_loc3[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
-        #
-        # self.fc_loc4[2].weight.data.zero_()
-        # self.fc_loc4[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
-        #
-        # self.fc_loc5[2].weight.data.zero_()
-        # self.fc_loc5[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
 
     # Spatial transformer network forward function
     def stn(self, x, x_last, x_pred):
@@ -103,31 +77,19 @@ class STN(nn.Module):
         theta1 = theta1.view(-1, 2, 3)
         theta2 = self.fc_loc2(xs[:, 250:500])
         theta2 = theta2.view(-1, 2, 3)
-        # theta3 = self.fc_loc3(xs)
-        # theta3 = theta3.view(-1, 2, 3)
-        # theta4 = self.fc_loc4(xs)
-        # theta4 = theta4.view(-1, 2, 3)
-        # theta5 = self.fc_loc5(xs)
-        # theta5 = theta5.view(-1, 2, 3)
 
         grid1 = F.affine_grid(theta1, x_last.size(), align_corners=True)
         x_pred[:, :, 0, :, :] = F.grid_sample(x_last, grid1, align_corners=True)
         grid2 = F.affine_grid(theta2, x_last.size(), align_corners=True)
         x_pred[:, :, 1, :, :] = F.grid_sample(x_last, grid2, align_corners=True)
-        # grid3 = F.affine_grid(theta3, x_last.size(), align_corners=True)
-        # x_pred[:, :, 2, :, :] = F.grid_sample(x_last, grid3, align_corners=True)
-        # grid4 = F.affine_grid(theta4, x_last.size(), align_corners=True)
-        # x_pred[:, :, 3, :, :] = F.grid_sample(x_last, grid4, align_corners=True)
-        # grid5 = F.affine_grid(theta5, x_last.size(), align_corners=True)
-        # x_pred[:, :, 4, :, :] = F.grid_sample(x_last, grid5, align_corners=True)
 
         return x_pred
 
     def forward(self, x, x_flow, x_pred):
         # transform the input
         x_last = x[:, :, -1, :, :]
-        x = x.view(x.shape[0], -1, 256, 512)
-        x_flow = x_flow.view(x_flow.shape[0], -1, 256, 512)
+        x = x.view(x.shape[0], -1, 200, 320)
+        x_flow = x_flow.view(x_flow.shape[0], -1, 200, 320)
         x = torch.cat((x, x_flow), dim=1)
 
         x = self.stn(x, x_last, x_pred)
